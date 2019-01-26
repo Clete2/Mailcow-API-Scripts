e12a0bb83f4da2790c76fd2129f52e441d85b845
Clete Blackwell II <clete@blackwell.email>
2019-01-25 21:30:47 -0500

    Add nonzero status code for errors

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import quota from './quota';
 const isQuarantined = quarantine();
 const isCloseToExceedingQuota = quota();
 
-if (isQuarantined || isCloseToExceedingQuota) {
+if (isQuarantined === true || isCloseToExceedingQuota === true) {
   console.log('A status check failed. Exiting with non-zero status code.');
   process.exit(1);
 }
